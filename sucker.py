93e5065543fd3d277cfb03fc8eb846948032dd4d
Jonathan Bowman <git@bowmanjd.com>
2021-10-01 09:40:26 -0400

    feat: adjust to use SF field names, write output file

--- sucker.py
+++ sucker.py
@@ -2,12 +2,15 @@
 
 import csv
 import mimetypes
+import pathlib
+import re
 import signal
 import sys
+import typing
 from concurrent.futures import ThreadPoolExecutor
 from functools import partial
-from pathlib import Path
 from threading import Event
+from urllib.parse import urlparse
 from urllib.request import urlopen
 
 from rich.progress import (
@@ -33,6 +36,8 @@ progress = Progress(
     auto_refresh=False,
 )
 
+unwanted_characters = re.compile("[^A-Za-z0-9_-]+")
+
 
 OUT_DIR = "out"
 
@@ -52,22 +57,26 @@ def signal_handler(signal_number, frame):  # type: ignore
 signal.signal(signal.SIGINT, signal_handler)
 
 
-def copy_url(task_id: TaskID, name: str, sku: str, url: str) -> None:
+def copy_url(
+    task_id: TaskID, file_stem: str, url: str, extension: typing.Optional[str]
+) -> None:
     """Copy data from a url to a local file.
 
     Args:
         task_id: task id for progress bar
-        name: Product name
-        sku: Product sku
+        file_stem: name of file to use, without extensions
         url: Image URL
+        extension: optional explicit file extension
     """
     progress.console.log(f"Requesting {url}")
     response = urlopen(url)
-    content_type = response.info()["content-type"]
-    extension = mimetypes.guess_extension(content_type)
-    file_path = Path(
-        f"{OUT_DIR}/{'_'.join(name.split()).lower()}-{sku.strip()}{extension}"
-    )
+    if not extension:
+        content_type = response.info()["content-type"]
+        extension = mimetypes.guess_extension(content_type)
+        progress.console.log(
+            f"No extension specified; inferring {extension} from Content Type."
+        )
+    file_path = pathlib.Path(f"{OUT_DIR}/{file_stem}{extension}")
     progress.update(task_id, total=int(response.info()["Content-length"]))
     with file_path.open("wb") as dest_file:
         progress.start_task(task_id)
@@ -85,19 +94,42 @@ def download(csv_file: str) -> None:
     Args:
         csv_file: name of CSV file to parse
     """
-    Path(OUT_DIR).mkdir(exist_ok=True)  # ensure the "out" directory exists
+    pathlib.Path(OUT_DIR).mkdir(exist_ok=True)  # ensure the "out" directory exists
 
-    in_path = Path(csv_file)
+    in_path = pathlib.Path(csv_file)
+    out_path = in_path.with_stem(f"importable_{in_path.stem}")
 
     with progress:
         with ThreadPoolExecutor(max_workers=8) as pool:
-            with in_path.open("r", newline="", encoding="utf-8-sig") as in_file:
+            with in_path.open(
+                "r", encoding="utf-8-sig", newline=""
+            ) as in_file, out_path.open(
+                "w", encoding="utf-8-sig", newline=""
+            ) as out_file:
                 reader = csv.DictReader(in_file)
+                writer = csv.DictWriter(out_file, ["Id", "Image_URL__c"])
+                writer.writeheader()
                 for row in reader:
-                    task_id = progress.add_task(
-                        "download", filename=row["Name"], start=False, visible=False
+                    file_stem = "_".join(row["Name"].split()).lower()
+                    file_stem = unwanted_characters.sub("", file_stem)
+                    file_stem = "-".join([file_stem, row["StockKeepingUnit"].strip()])
+                    original_path = pathlib.PurePosixPath(
+                        urlparse(row["Image_URL__c"]).path
                     )
-                    pool.submit(copy_url, task_id, row["Name"], row["SKU"], row["URL"])
+                    extension = original_path.suffix
+                    task_id = progress.add_task(
+                        "download", filename=file_stem, start=False, visible=True
+                    )
+
+                    new_row = {
+                        "Id": row["Id"],
+                        "Image_URL__c": f"{file_stem}{extension}",
+                    }
+                    writer.writerow(new_row)
+
+                    pool.submit(
+                        copy_url, task_id, file_stem, row["Image_URL__c"], extension
+                    )
 
 
 def run() -> None:
